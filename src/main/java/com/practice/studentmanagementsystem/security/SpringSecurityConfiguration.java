00115aa331c3e35b455f00dff3a63737645a719e
Sandeep Singh <singh.sandi10@gmail.com>
2023-06-26 10:48:21 -0400

    Making todo an entity

--- src/main/java/com/practice/studentmanagementsystem/security/SpringSecurityConfiguration.java
+++ src/main/java/com/practice/studentmanagementsystem/security/SpringSecurityConfiguration.java
@@ -1,14 +1,18 @@
 package com.practice.studentmanagementsystem.security;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 import java.util.function.Function;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SpringSecurityConfiguration {
@@ -19,7 +23,7 @@ public class SpringSecurityConfiguration {
 		UserDetails userDetails1 = createNewUser("sandy", "dummy");
 		UserDetails userDetails2 = createNewUser("sodhi", "dummy");
 
-		return new InMemoryUserDetailsManager(userDetails1,userDetails2);
+		return new InMemoryUserDetailsManager(userDetails1, userDetails2);
 	}
 
 	private UserDetails createNewUser(String username, String password) {
@@ -33,5 +37,22 @@ public class SpringSecurityConfiguration {
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
+	}
+
+	// All URLs are protected
+	// A login form is shown for unauthorized requests
+	// CSRF disable
+	// Frames
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+		http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated());
+		http.formLogin(withDefaults());
+
+		http.csrf().disable();
+		http.headers().frameOptions().disable();
+
+		return http.build();
 	}
 }

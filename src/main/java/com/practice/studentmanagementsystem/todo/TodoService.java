f645d15e8a2415d3ce224497cf608ec0a821cc9f
Sandeep Singh <singh.sandi10@gmail.com>
2023-06-25 21:05:23 -0400

    Implementing update todo 2

--- src/main/java/com/practice/studentmanagementsystem/todo/TodoService.java
+++ src/main/java/com/practice/studentmanagementsystem/todo/TodoService.java
@@ -7,6 +7,8 @@ import java.util.function.Predicate;
 
 import org.springframework.stereotype.Service;
 
+import jakarta.validation.Valid;
+
 @Service
 public class TodoService {
 private static List<Todo> todos = new ArrayList<>();
@@ -32,5 +34,15 @@ private static List<Todo> todos = new ArrayList<>();
 		todos.removeIf(predicate);
 	}
 
+	public void updateTodo(@Valid Todo todo) {
+		deleteById(todo.getId());
+		todos.add(todo);
+	}
+	
+	public Todo findById(int id) {
+		Predicate<? super Todo> predicate = todo -> todo.getId() == id;
+		Todo todo = todos.stream().filter(predicate).findFirst().get();
+		return todo;
+	}
 	
 }
